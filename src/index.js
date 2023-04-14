418196b64eab1af6606d775923c687a64eb4fd93
kanyi <107926388+jkanyi-web@users.noreply.github.com>
2023-04-14 17:16:44 +0300

    hit-api full functionality

--- src/index.js
+++ src/index.js
@@ -3,5 +3,19 @@
 import './style.css';
 
 import callApi from './modules/hitApi.js';
+import postData from './modules/postData.js';
+import receiveData from './modules/receiveData.js';
+
+const form = document.querySelector('form');
+const refreshBtn = document.getElementById('refresh');
 
 callApi();
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  postData(e);
+});
+
+refreshBtn.addEventListener('click', () => {
+  receiveData();
+});
